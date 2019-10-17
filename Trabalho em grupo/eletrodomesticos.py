f66ed2b95ab20112677e97db0cdd4304b19a94fa
jaquelinekraemer <55961924+jaquelinekraemer@users.noreply.github.com>
2019-10-17 17:23:38 -0300

    Add files via upload
    Arquivos atualizados

---
+++ Trabalho em grupo/eletrodomesticos.py
@@ -0,0 +1,13 @@
+class Eletrodomesticos:
+    def __init__(self, tipo, marca,modelo,data_fabric,preco,serie):
+        self.id = 0
+        self.marca = marca
+        self.modelo = modelo
+        self.data_fabric = data_fabric
+        self.preco = preco
+        self.serie = serie
+        self.tipo = tipo
+
+     
+
+
--- Trabalho em grupo/trabalhinho.py
+++ Trabalho em grupo/trabalhinho.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, redirect, request
+from eletrodomesticos import Eletrodomesticos
+import MySQLdb
 
-titulo = "Página interna XX Eletrodomésticos"
+def salvar_eletro(cadastro_geral):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae12", passwd="grupo07", database="zuplae12")
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO eletrodomestico (marca, modelo, data_fabric, preco, serie, tipo)" + 
+    " VALUES ('{}','{}', '{}', '{}','{}','{}')".format(cadastro_geral.marca, cadastro_geral.modelo, cadastro_geral.data_fabric, cadastro_geral.preco,cadastro_geral.serie, cadastro_geral.tipo))
+    conexao.commit()
+    conexao.close()
+
+def listar_eletro_db():
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae12", passwd="grupo07", database="zuplae12")
+    cursor = conexao.cursor()
+    cursor.execute("select * from eletrodomestico") 
+    cadastro_geral = []
+    for i in cursor.fetchall():
+        produto = Eletrodomesticos(i[6], i[1], i[2], i[3], i[4], i[5])
+        cadastro_geral.append(produto)        
+    return cadastro_geral
+
+titulo = "Página interna"
 
 app=Flask(__name__)
 
@@ -13,12 +33,28 @@ def menu():
     return render_template('menu.html', titulo=titulo)
 
 @app.route('/cad_produto')
-def cad_produto():
+def cadastrar():
     return render_template('cad_produto.html', titulo = titulo)
 
+@app.route('/lista_produto')
+def listar_produtos():
+    return render_template('lista_produto.html', lista = listar_eletro_db())
+    
 @app.route('/cad_produto/salvar', methods=['POST'])
-def salvar_produto():
-    return redirect('/cad_produto/lista_produto')
+def salvar_eletrodomestico():
+    tipo = request.form['tipo']
+    marca = request.form['marca']
+    modelo = request.form['modelo']
+    data_fabric = request.form['data_fabric']
+    preco = request.form['preco']
+    serie = request.form['serie']
+    cadastro_geral = Eletrodomesticos(tipo, marca, modelo, data_fabric, preco, serie)
+    salvar_eletro(cadastro_geral)
+    return render_template('Cadastro salvo com sucesso!')
+
+@app.route('/cad_produto/alterar')
+def alterar():
+    return render_template('lista_produto.html')
 
 @app.route('/efetuar_compra')
 def efetuar_compra():
@@ -30,4 +66,4 @@ def politica_usuario():
 
 
 
-app.run()
+app.run(debug=True)
