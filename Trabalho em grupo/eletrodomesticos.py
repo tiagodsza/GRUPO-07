94d0f93a8ec1017267fb465c9751a438499c85ee
tiagodsza <49600413+tiagodsza@users.noreply.github.com>
2019-10-20 18:59:15 -0300

    Add files via upload

--- Trabalho em grupo/eletrodomesticos.py
+++ Trabalho em grupo/eletrodomesticos.py
@@ -1,11 +1,10 @@
 class Eletrodomesticos:
-    def __init__(self, tipo, marca,modelo,data_fabric,preco,serie):
-        self.id = 0
+    def __init__(self, tipo, marca, modelo, data_fabric, preco):
+        self.serie = 0
         self.marca = marca
         self.modelo = modelo
         self.data_fabric = data_fabric
         self.preco = preco
-        self.serie = serie
         self.tipo = tipo
 
      
--- Trabalho em grupo/trabalhinho.py
+++ Trabalho em grupo/trabalhinho.py
@@ -1,17 +1,83 @@
 from flask import Flask, render_template, redirect, request
 from eletrodomesticos import Eletrodomesticos
+from comprar import Comprar
 from usuario_senha import Usuario_senha
 import MySQLdb
 
+titulo = "Página interna"
 
-conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae12", passwd="grupo07", database="zuplae12")
-cursor = conexao.cursor()
-cursor.execute('SELECT * FROM usuario_senha')
-lista_usuarios = []
-for i in cursor.fetchall():
-    usuario = Usuario_senha(i[1],i[2])   
-    lista_usuarios.append(usuario)
-    print(usuario.senha)
+app=Flask(__name__)
+
+@app.route('/')
+
+def inicio():
+    return render_template('login.html', titulo = titulo)
+
+
+
+
+@app.route('/testar_login', methods=['POST'])
+def login():
+
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae12", passwd="grupo07", database="zuplae12")
+    cursor = conexao.cursor()
+    cursor.execute('SELECT * FROM usuario_senha')
+    lista_usuarios = []
+    for i in cursor.fetchall():
+        usuario = Usuario_senha(i[1],i[2])   
+        lista_usuarios.append(usuario)
+
+    usuario1 = request.form['usuario']
+    senha1 = request.form['senha']
+    user = Usuario_senha(usuario1,senha1)
+    for i in range(len(lista_usuarios)):
+        var_temp_usuario = lista_usuarios[i]
+        if user.senha == var_temp_usuario.senha and user.usuario == var_temp_usuario.usuario:
+            return render_template('menu.html', titulo=titulo)
+
+    if user.senha != var_temp_usuario.senha:
+            return redirect('/')
+
+    elif user.usuario != var_temp_usuario.usuario:
+            return redirect('/')  
+
+def listar_compras_db():
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae12", passwd="grupo07", database="zuplae12")
+    cursor = conexao.cursor()
+    cursor.execute("select * from compra")
+    compras_listadas = []
+    for i in cursor.fetchall():
+        compra = Comprar(i[1], i[2], i[3], i[4])
+        compras_listadas.append(compra)
+    return compras_listadas
+
+@app.route('/salvar_compras', methods=['POST'])
+def salvar_comprar():
+    nome = request.form['nome']
+    cpf = request.form['cpf']
+    endereco = request.form['endereco']
+    serie = request.form['serie']
+    serie_fatiada = serie.strip().split(',')
+#Abaixo, tivemos que criar outra váriavel chamada serie_inteiro para transformar em int o que veio do html
+    serie_inteiro = int(serie_fatiada[2])
+    sales_order = Comprar(nome, cpf, endereco, serie_inteiro)    
+    lista = listar_compras_db()
+    for i in lista:
+        if sales_order.serie_fk == i.serie_fk:
+            return render_template('erro_compra.html', titulo = titulo)
+    salvar_compras(sales_order)
+    return redirect('/lista_compras')
+
+@app.route('/lista_compras')
+def lista_compras():
+    return render_template('lista_compras.html', lista = listar_compras_db())
+        
+def salvar_compras(sales_order):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae12", passwd="grupo07", database="zuplae12")
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO compra (nome,cpf,endereco,serie_fk) VALUES ('{}', '{}', '{}', '{}')".format(sales_order.nome, sales_order.cpf,sales_order.endereco,sales_order.serie_fk))
+    conexao.commit()
+    conexao.close()
 
 def salvar_eletro(cadastro_geral):
     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae12", passwd="grupo07", database="zuplae12")
@@ -27,17 +93,11 @@ def listar_eletro_db():
     cursor.execute("select * from eletrodomestico") 
     cadastro_geral = []
     for i in cursor.fetchall():
-        produto = Eletrodomesticos(i[6], i[1], i[2], i[3], i[4], i[5])
+        produto = Eletrodomesticos(i[1], i[2], i[3], i[4], i[5])
+        produto.serie = i[0]
         cadastro_geral.append(produto)        
     return cadastro_geral
 
-titulo = "Página interna"
-
-app=Flask(__name__)
-
-@app.route('/')
-def inicio():
-    return render_template('login.html', titulo = titulo)
 
 # @app.route
 # def validar_usuario_senha():
@@ -50,27 +110,11 @@ def inicio():
 #             menu()
 #             print('usuario validado com sucesso')
 #         else:
-#             print("voce digitou a senha errada")
-
-@app.route('/testar_login', methods=['POST'])
-def login():
-    usuario1 = request.form['usuario']
-    senha1 = request.form['senha']
-    user = Usuario_senha(usuario1,senha1)
-    for i in range(len(lista_usuarios)):
-        var_temp_usuario = lista_usuarios[i]
-        if user.senha == var_temp_usuario.senha and user.usuario == var_temp_usuario.usuario:
-            return render_template('menu.html', titulo=titulo)
-
-    if user.senha != var_temp_usuario.senha:
-            return redirect('/')
-
-    elif user.usuario != var_temp_usuario.usuario:
-            return redirect('/')     
+#             print("voce digitou a senha errada") 
 
 @app.route('/menu')
 def menu():
-    return render_template('menu.html', titulo=titulo)
+    return render_template('menu.html', titulo = titulo)
 
 @app.route('/cad_produto')
 def cadastrar():
@@ -87,10 +131,9 @@ def salvar_eletrodomestico():
     modelo = request.form['modelo']
     data_fabric = request.form['data_fabric']
     preco = request.form['preco']
-    serie = request.form['serie']
-    cadastro_geral = Eletrodomesticos(tipo, marca, modelo, data_fabric, preco, serie)
+    cadastro_geral = Eletrodomesticos(tipo, marca, modelo, data_fabric, preco)
     salvar_eletro(cadastro_geral)
-    return render_template('Cadastro salvo com sucesso!')
+    return redirect('/lista_produto')
 
 @app.route('/cad_produto/alterar')
 def alterar():
@@ -98,7 +141,7 @@ def alterar():
 
 @app.route('/efetuar_compra')
 def efetuar_compra():
-    return render_template('efetuar_compra.html', titulo = titulo)
+    return render_template('efetuar_compra.html', titulo = titulo, lista = listar_eletro_db())
 
 @app.route('/politica_uso')
 def politica_usuario():
